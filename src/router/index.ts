3e65ddc2748357054cd25bced6dabf9f82579fdd
SamuelVanhaecke <samuel.vanhaecke@student.howest.be>
2023-12-13 10:47:05 +0100

    feat: added tailwindcss

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       component: () => import('../views/dashboard.vue'),
-      
-      
     },
-    
+    {
+      path: '/onboarding',
+      component: () => import('../views/onboarding.vue'),
+    },
+
     {
       path: '/:pathMatch(.*)*',
-      component: () => import('../views/NotFound.vue')
-    }
-  ]
+      component: () => import('../views/NotFound.vue'),
+    },
+  ],
 })
 
 export default router
---
+++ src/views/onboarding.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="bg-red-500">
+    <h1 class="text-3xl font-bold underline">Welcome to our App!</h1>
+    <p>Start by calibrating your gyroscope:</p>
+    <div ref="gyroContainer" class="gyro-container">
+      <div ref="gyro" class="gyro"></div>
+      <div class="gyro-indicator"></div>
+      <button @click="">Calibrate</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped>
+.gyro-container {
+  width: 200px;
+  height: 200px;
+  position: relative;
+}
+
+.gyro {
+  width: 100%;
+  height: 100%;
+  background-color: #000;
+  border-radius: 50%;
+  transition: transform 0.1s ease;
+}
+
+.gyro-indicator {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 16px;
+  height: 16px;
+  background-color: #f00;
+  border-radius: 50%;
+}
+</style>
