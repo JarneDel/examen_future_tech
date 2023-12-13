48a27f106005cc945d19ce4588c0c40a847ed919
SamuelVanhaecke <samuel.vanhaecke@student.howest.be>
2023-12-13 12:56:06 +0100

    feat: gyroposition works + reset

--- src/components/TestBluetooth.vue
+++ src/components/TestBluetooth.vue
@@ -1,36 +1,31 @@
 <script setup lang="ts">
-  import { useBle } from '../composables/useBle.ts'
+import { useBle } from '../composables/useBle.ts'
 
-  const { enableNotifications, listen } = useBle()
-  const activate = async () => {
-    enableNotifications().then(() => {
-      listen((gyro, acc, mag) => {
-        console.log(gyro, acc, mag)
-      })
+const { enableNotifications, listen } = useBle()
+const activate = async () => {
+  enableNotifications().then(() => {
+    // @ts-ignore
+    listen((gyro, acc, mag) => {
+      // console.log(gyro, acc, mag)
     })
-    // const service = await getPrimaryService()
-    // console.log(service)
-    // if (service){
-    //   setInterval(async () => {
-    //     const characteristic = await readCharacteristic()
-    //     characteristic.startNotifications().then(value => {
-    //       value.readValue().then(value => {
-    //         console.log(value)
-    //       })
-    //     })
-    //   }, 1000)
-    // }
-
-  }
+  })
+  // const service = await getPrimaryService()
+  // console.log(service)
+  // if (service){
+  //   setInterval(async () => {
+  //     const characteristic = await readCharacteristic()
+  //     characteristic.startNotifications().then(value => {
+  //       value.readValue().then(value => {
+  //         console.log(value)
+  //       })
+  //     })
+  //   }, 1000)
+  // }
+}
 </script>
 
 <template>
-<button @click="activate">
-  Init ble
-
-</button>
+  <button @click="activate">Init ble</button>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/views/onboarding.vue
+++ src/views/onboarding.vue
@@ -16,9 +16,58 @@
       <div
         class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-4 h-4 bg-blue-500 rounded-full"
       ></div>
+      <p>{{ positionX }}</p>
+      <p>{{ positionY }}</p>
       <button @click="">Calibrate</button>
     </div>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue'
+import { useBle } from '../composables/useBle'
+
+const { gyro } = useBle()
+
+const positionX = ref(0)
+const positionY = ref(0)
+
+const gyroXZero = ref(0)
+const gyroYZero = ref(0)
+
+// run function to calculate position from acc data (reset position with space)
+// use setInterval to run function every 100ms
+// use onMounted and onUnmounted to start and stop the interval
+
+const calculatePosition = () => {
+  // position is calculated from the acc data, when holding the device still, the position should stay the same and should not go to 0
+  // when the device is moved, the position should change accordingly
+  // when the device is moved back to the original position, the position should go back to the original position
+  // consider gyroXZero and gyroYZero as the original position
+
+  positionX.value = gyro.value.x - gyroXZero.value
+  positionY.value = gyro.value.y - gyroYZero.value
+}
+
+// function to calibrate the position
+// use the current position as the new zero position
+// use a button to call the function
+
+const calibrate = () => {
+  gyroXZero.value = gyro.value.x
+  gyroYZero.value = gyro.value.y
+}
+
+// listen to space bar to calibrate the position
+// use onMounted and onUnmounted to start and stop the event listener
+
+onMounted(() => {
+  setInterval(calculatePosition, 100)
+
+  window.addEventListener('keydown', e => {
+    if (e.code === 'Space') {
+      calibrate()
+    }
+  })
+})
+</script>
