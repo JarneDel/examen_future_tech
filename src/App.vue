c7d09db2f6a5e55d65c5069e2b87e09c1bf34e3a
JarneDel <jarne.delarue@student.howest.be>
2023-12-13 12:06:02 +0100

    ble 2.0

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import TestBluetooth from './components/TestBluetooth.vue'
 
 
 </script>
 
 <template>
   <RouterView />
-  <TestBluetooth/>
 </template>
 
 <style scoped>
