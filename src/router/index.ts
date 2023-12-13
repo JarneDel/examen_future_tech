f6872daa0dccd63663306fb5cc8f020217316840
Lei <lei.jin@student.howest.be>
2023-12-13 10:58:11 +0100

    feat: add a test view for firestore

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      component: () => import('../views/dashboard.vue'),
-      
-      
+      component: () => import('../views/Dashboard.vue'),
     },
-    
+
+    {
+      path: '/firestore',
+      component: () => import('../views/Firestore.vue'),
+    },
+
     {
       path: '/:pathMatch(.*)*',
-      component: () => import('../views/NotFound.vue')
-    }
-  ]
+      component: () => import('../views/NotFound.vue'),
+    },
+  ],
 })
 
 export default router
